3001aa22e5e133995ac6cd35b3cf06b75b1c2e6f
Andreas Jud <andreas.jud@gmail.com>
2024-07-23 06:30:21 +0200

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,9 @@
+# Building a Docker Image
+docker build -t my-docker-image .
+
+# Running a Docker Container
+docker run -d --name my-docker-container -p 8000:8000 -v .:/app my-docker-image
+
+# Running a Docker Container from a Docker Compose file
+docker compose up -d
+
